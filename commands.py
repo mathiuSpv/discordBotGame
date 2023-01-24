a6fc32462d4f89ada7ed9154c7b7620e1b76056a
mathiuS <mathius998@gmail.com>
2023-01-24 17:47:11 -0300

    commit simple

--- commands.py
+++ commands.py
@@ -3,12 +3,15 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 
-class discordBot(discord.Client):
+class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
-        
+
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if "$start" == message.content.lower():
-            await message.channel.send("Funcaaaaaaaa")
+        if message.content.startswith('$start'):
+            await self.user.dm_channel('Hello!')
+            
+
+client = MyClient(intents=intents)
