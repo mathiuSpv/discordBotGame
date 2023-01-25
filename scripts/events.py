f24a0104d5647fc0899fe2f1e2aeb416e96b7883
mathiuS <mathius998@gmail.com>
2023-01-25 19:05:37 -0300

    new update

---
+++ scripts/events.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+
+intents= discord.Intents.default()
+intents.message_content = True
+client= commands.Bot(command_prefix='$', intents= intents)
+status= cycle(["Morning", "Midday", "Night"])
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity= discord.Game(name= "$start - DSRoleplay" ))
+    await appearance.start()
+    print(f'{client.user} is on!')
+
+@tasks.loop(hours= 2, minutes= 30)
+async def appearance():
+    channel= client.get_channel(1055904937575448587)
+    await channel.send(f"time day (game): {next(status)}")
+
+
+if __name__ == "__main__":
+    with open("token.txt", mode= "r") as filetext:
+        token= filetext.readline()
+        filetext.close()
+    client.run(token)
