fdd7e4b64318031ba1bed57d7debe4fe523b9f9b
mathiuS <mathius998@gmail.com>
2023-02-01 19:31:40 -0300

    new update

--- scripts/commands.py
+++ scripts/commands.py
@@ -7,46 +7,75 @@ intents= discord.Intents.default()
 intents.message_content = True
 client= commands.Bot(command_prefix='$', intents= intents)
 
+def read_data():
+    players_system= dict()
+    with open("./datauser.csv", mode= 'r') as file:
+        players_list= file.readlines()
+    for user_id, user_name in players_list:
+        players_system[f'{user_id}']= user_name
+    return players_system
+
+players= read_data()
+client.change_presence(activity= discord.Game(name= "$start - DSRoleplay" ))
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity= discord.Game(name= "$start - DSRoleplay" ))
     print(f'{client.user} is on!')
 
 
-@client.command()
-async def start(ctx):
-    """First steps of registation function"""
-
-    await ctx.message.add_reaction('\U0001F47E')
-    msg = await ctx.author.send(
-"""```Para poder registrarte escribeme:\n
-    **$rg username**\n
-Registrado, debes asignar tus puntos:\n
-    **$player stats**```""")
+@client.command(aliases= ["rg" ,"reg" ,"start"])
+async def registration(ctx, *, username= ""):
     
-    def check(message):
-        return "$rg" in message.content and message.channel == msg.channel
-
-    try:
-        msg = await client.wait_for('message' , timeout= 30, check= check)
-        if msg: await ctx.author.send("Well done!")
-    except asyncio.TimeoutError:
-        await ctx.author.send("Time Out")
+    async def regist_user(user_id: str, user_name: str):
+        with open("./datausers.csv", mode= "a") as file:
+            file.write(f"{user_id};{user_name}\n")
+            file.close()
+        players[f'{user_id}']= user_name
+    
+    async def error_username(username: str):
+        if username == "":
+            await ctx.author.send("You need a username")
+        elif " " in username:
+            await ctx.author.send("A username cannot have spaces")
+        elif username in players.values():
+            await ctx.author.send("A username is already used")
+        else:
+            await ctx.author.send("A username should be only have letters ")
+            
+    
+    if ctx.author.id in players:
+        await ctx.author.send("You are already register!")
+        return
     else:
-        pass
+        if username.isalpha():
+            await ctx.message.add_reaction('\U0001F44D')
+            await regist_user(ctx.author.id, username , "None")
+            await ctx.author.send("Welcome to a news adventures")
+        else:
+            await ctx.message.add_reaction('\U0001F44E')
+            await error_username(username)
+            await ctx.author.send("Remember you can only user Letter as a user name")
 
-@client.command(aliases= ["rg"])
-@start.after_invoke
-async def registration(ctx, *, arg= ""):
-    if arg.isalpha():
-        await ctx.author.send("Registro completado, asigna tus puntos y empeza a jugar")
-    else:
-        await ctx.author.send("El username solo deben ser letras")
-        
-@registration.error
-async def registation_errors(ctx, error):
-    print(error)
+# @client.command()
+# async def start(ctx):
+#     """First steps of registation function"""
+
+#     await ctx.message.add_reaction('\U0001F47E')
+#     await ctx.author.send(
+# """```\n
+#     **$rg username**\n
+#     \n
+#     **$player stats**```""")
+    
+#     def validate_username(message):
+#         print(message)
+#         return message.startswith("$rg") or message.startswith("$registration") and message.channel == ctx.channel
+    
+#     try:
+#         msg= await client.wait_for('message' , timeout= 30, check= validate_username)
+#         print(msg)
+#     except asyncio.TimeoutError:
+#         await ctx.author.send("Time Out")
 
 
 @client.command()
