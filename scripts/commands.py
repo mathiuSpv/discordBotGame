28c16026cb66e79787241f80bdc57d08bf94ccc4
mathiuS <mathius998@gmail.com>
2023-02-16 09:57:47 -0300

    new update

--- scripts/commands.py
+++ scripts/commands.py
@@ -1,20 +1,15 @@
 import discord
-import asyncio
-import datetime
 from discord.ext import commands
 from dataScript import player
 
 intents= discord.Intents.default()
 intents.message_content = True
 client= commands.Bot(command_prefix='$', intents= intents)
-register_status= {}
-
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity= discord.Game(name= "Andventure - Try to play \U000027A1  $start" ))
+    await client.change_presence(activity= discord.Game(name= "Andventure - PLAY RPG \U000027A1  $newstart" ))
     print(f'{client.user} is on!')
-
 
 @client.command(aliases= ["clear"])
 @commands.has_permissions(administrator= True)
@@ -24,45 +19,37 @@ async def cls(ctx):
         if not message.pinned:
             msg_list.append(message) 
     await ctx.channel.delete_messages(msg_list)
+    
+@client.command()
+async def deletedb(ctx):
+    founders= [395647730112004107] 
+    if ctx.author.id in founders:
+        player.delete_db()
+        await ctx.channel.send("All data base clear!")
 
 @client.command()
-async def start(ctx, time= 60):
-    user_id= ctx.author.id
-    register_status[user_id]= True
-    await ctx.author.send("Para registrater debes colocar $register 'nombre'!\n (El comando se desabilita al pasar 1 minuto)")
-    await asyncio.sleep(time)
-    register_status[user_id]= False
-    print(register_status)
-    if register_status[user_id] == False:
-        del register_status[user_id]
-        await ctx.author.send("Time out!")
-
-@client.command(aliases= ["rg" ,"reg"])
-async def register(ctx):
-    async def __error(*args, **kwargs):
-        error= False
-        if player.get_user(ctx.author.id):
-            error= True
-            await ctx.author.send("You are already registered")
-        if error:
-            await ctx.message.add_reaction('\U000026D4')
-            return error
-        else:
-            await ctx.message.add_reaction('\U0001F197')
-            return error
+async def newstart(ctx):
+    def __error(*args, **kwargs):
+        if player.get_user(user_id):
+            return True
+        return False
     
     user_id= ctx.author.id
-    if user_id in register_status:
-        if not __error():
-            player.new_player(user_id)
-            del register_status[user_id]
-        await ctx.author.send("Welcome new adventurer")
+    if not __error(user_id):
+        text= """ANDVENTURE is a RPG Turn in Discord Game\n
+        You have a random enemys, raids and rare loot \n
+        First off, you must assing your Stats Points: \n
+        **$stats**"""
+        embed= discord.Embed(title="__Welcome new Adventurer!__", color=0x9F00FF ,description= text)
+        player.new_player(user_id)
+        await ctx.author.send(embed= embed)
     else:
-        await ctx.author.send("No")
+        await ctx.author.send("```You are already registered```")
+        await ctx.message.add_reaction('\U000026D4')
         
 @client.command()
 async def mycommand(ctx):
-    embed = discord.Embed(title="Título del Embed", description="Descripción del Embed", color=0xFF5733)
+    embed = discord.Embed(title="Título del Embed", description="Descripción del Embed", color=0x9F00FF)
     await ctx.send(embed=embed)
 
 
@@ -71,3 +58,10 @@ if __name__ == "__main__":
         token= filetext.readline()
         filetext.close()
     client.run(token)
+
+
+# await asyncio.sleep(time)
+#     register_status[user_id]= False
+#     if register_status[user_id] == False:
+#         del register_status[user_id]
+#         await ctx.author.send("Time out! Try again $newstart for register!")
--- scripts/dataScript.py
+++ scripts/dataScript.py
@@ -1,30 +1,29 @@
 from sqlalchemy import create_engine, Column, Integer, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
 
 Base= declarative_base()
 
 class Player(Base):
     __tablename__= 'players'
     discord_id= Column(Integer, primary_key= True)
+    level= relationship('PlayerLevel')
+    stats= relationship('PlayerStats')
 
 class PlayerLevel(Base):
     __tablename__= 'players_level'
     discord_id= Column(Integer, ForeignKey('players.discord_id'), primary_key= True)
-    player_r_table= relationship('players', back_populates= 'players_level')
-    level= Column(Integer)
+    n_level= Column(Integer)
     experience= Column(Integer)
     stats_points= Column(Integer)
     
 class PlayerStats(Base):
     __tablename__= 'players_stats'
     discord_id= Column(Integer, ForeignKey('players.discord_id'), primary_key= True)
-    player_r_table= relationship('players', back_populates= 'players_stats')
-    vigor = Column(Integer)
-    endurance = Column(Integer)
-    strenght = Column(Integer)
-    dexterity = Column(Integer)
-    intelligence = Column(Integer)
+    vigor= Column(Integer)
+    endurance= Column(Integer)
+    strenght= Column(Integer)
+    dexterity= Column(Integer)
+    intelligence= Column(Integer)
 
 class FunctionsPlayers():
     """Methods from Players"""
@@ -33,54 +32,64 @@ class FunctionsPlayers():
 
     def new_player(self, player_id):
         """Create a data of new player: player_id and player_stats"""
-        player = Player(player_discord_id= player_id)
-        player_level= PlayerLevel(discord_id= player_id,
-                                  level= 1, experience= 0, stats_points= 9)
-        player_stats= PlayerStats(discord_id= player_id,
-                                  player_vig= 1, player_end= 1, player_str= 1, player_dex= 1, player_int= 1)
-        self.session.add(player); self.session.add(player_level); self.session.add(player_stats)
+        player = Player(discord_id= player_id)
+        player.level= [PlayerLevel(n_level= 1, experience= 0, stats_points= 9)]
+        player.stats= [PlayerStats(vigor= 1, endurance= 1, strenght= 1, dexterity= 1, intelligence= 1)]
+        self.session.add(player)
         self.session.commit()
-    
+        self.session.close()
+        
     def update_stats_info(self, player_id, player_stats: dict):
         """Must give a dictionary from .get_stats() modified"""
-        player= self.session.query(PlayerStats).filter_by(player_discord_id= player_id).first()
+        player= self.session.query(PlayerStats).filter_by(discord_id= player_id).first()
         player.vigor= player_stats['vig']
         player.endurance= player_stats['end']
         player.strenght= player_stats['str']
         player.dexterity= player_stats['dex']
         player.intelligence= player_stats['int']
         self.session.commit()
+        self.session.close()
         
     def update_level_info(self, player_id, player_level: dict):
         """Must give a dictionary from .get_level() modified"""
-        player= self.session.query(PlayerLevel).filter_by(player_discord_id= player_id).first()
-        player.level= player_level['lvl']
+        player= self.session.query(PlayerLevel).filter_by(discord_id= player_id).first()
+        player.n_level= player_level['lvl']
         player.experience= player_level['exp']
         player.stats_points = player_level['pts']
         self.session.commit()
+        self.session.close()
         
     def get_user(self, player_id):
         """Get a boolean if it user exits"""
-        player= self.session.query(Player).filter_by(player_discord_id= player_id).first()
-        if player:
+        if self.session.query(Player).filter(Player.discord_id== player_id).first():
             return True
         else:
             return False
     
     def get_level(self, player_id):
         """Get level as Dictionary with keys: lvl, exp, pts"""
-        player= self.session.query(PlayerLevel).filter_by(player_discord_id= player_id).first()
-        return {'lvl': player.level, 'exp': player.experience, 'pts': player.stats_points}
+        player= self.session.query(PlayerLevel).filter_by(discord_id= player_id).first()
+        self.session.close()
+        return {'lvl': player.n_level, 'exp': player.experience, 'pts': player.stats_points}
         
     def get_stats(self, player_id):
         """Get stats as Dictionary with keys: vig, end, str, dex, int"""
-        player= self.session.query(PlayerStats).filter_by(player_id= player_id).first()
+        player= self.session.query(PlayerStats).filter_by(discord_id= player_id).first()
+        self.session.close()
         return {'vig': player.vigor, 'end': player.endurance,
-                'str': player.strenght, 'dex': player.dexterity, 'int': player.intelligencet}
+                'str': player.strenght, 'dex': player.dexterity, 'int': player.intelligence}
+    
+    def delete_db(self):
+        """Delete all Player Data Base"""
+        self.session.query(Player).delete()
+        self.session.query(PlayerLevel).delete()
+        self.session.query(PlayerStats).delete()
+        self.session.commit()
+        self.session.close()
 
 
-engine= create_engine('sqlite:///database/players.db')
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session_player= Session()
-player= FunctionsPlayers(session_player)
+engine_player= create_engine('sqlite:///database/players.db')
+Base.metadata.create_all(engine_player)
+session_player = sessionmaker(bind=engine_player)
+session_player= session_player()
+player_db= FunctionsPlayers(session_player)
